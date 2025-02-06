d3bce3cda88d7d95b87e2d7caf8f5db595da5539
longlong A dog <45786195+qingshanyuluo@users.noreply.github.com>
2025-02-06 05:38:57 +0000

    只查询open的

--- blog/js/labels.80ef4637.js
+++ blog/js/labels.80ef4637.js
@@ -1,1 +1,162 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["labels"],{"1ead":function(e,t,n){},"32dc":function(e,t,n){"use strict";n.r(t);var l=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"page-labels"},[e.archives.labels.length?n("div",{staticClass:"nav flex flex-middle"},[n("div",{staticClass:"name flex flex-center flex-middle"},[e._v("标签")]),n("div",{staticClass:"labels flex-item flex"},e._l(e.archives.labels,(function(t){return n("a",{key:t.name,staticClass:"label flex flex-middle flex-center",class:[t.name===e.archives.label&&"active"],attrs:{href:"javascript:;"},domProps:{textContent:e._s(t.name)},on:{click:function(n){return e.changeLabel(t)}}})})),0)]):e._e(),n("div",{staticClass:"list"},[n("div",{staticClass:"item"},[e.archives.label?n("div",{staticClass:"item-name flex flex-middle"},[n("p",{domProps:{textContent:e._s(e.archives.label)}}),n("strong",{staticClass:"font-clg",domProps:{textContent:e._s("( "+e.archives.totalCount+" )")}})]):e._e(),n("ul",{staticClass:"archives"},e._l(e.archives.list,(function(t){return n("li",{key:t.number,staticClass:"archive flex flex-middle"},[n("span",{domProps:{textContent:e._s(e.formatTime(t.createdAt,"MM-dd"))}}),n("router-link",{attrs:{to:"/archives/"+t.number,title:t.title},domProps:{textContent:e._s(t.title)}}),n("div",{staticClass:"others flex-item flex-end flex flex-middle"},[n("i",{staticClass:"iconfont icon-comment"}),n("span",{domProps:{textContent:e._s(t.comments.totalCount)}})])],1)})),0)])]),e.archives.label?[e.archives.none?n("div",{staticClass:"auxi flex flex-middle flex-center"},[n("i",{staticClass:"iconfont icon-none"}),n("span",[e._v("目前就这么多啦~")])]):[e.archives.loading?[e._m(0)]:[n("div",{staticClass:"flex flex-middle flex-center"},[n("a",{staticClass:"btn-next flex flex-middle flex-center",attrs:{href:"javascript:;"},on:{click:e.getData}},[e._v("加载更多")])])]]]:e._e()],2)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"auxi flex flex-middle flex-center"},[n("i",{staticClass:"iconfont icon-loading"}),n("span",[e._v("正在加载中")])])}],s=(n("7f7f"),n("750b")),i=n("fa7d"),o={setup:function(e,t){var n=Object(s["e"])({list:[],labels:[],label:null,totalCount:0,cursor:null,loading:!1,none:!1}),l=function(){n.loading=!0;var e='query {\n          repository(owner: "qingshanyuluo", name: "qingshanyuluo.github.io") {\n            issues(filterBy: {labels: "'.concat(n.label,'"}, orderBy: {field: CREATED_AT, direction: DESC}, labels: null, first: 10, after: ').concat(n.cursor,") {\n              nodes {\n                title\n                createdAt\n                number\n                comments(first: null) {\n                  totalCount\n                }\n              }\n              pageInfo {\n                endCursor\n                hasNextPage\n              }\n              totalCount\n            }\n          }\n        }");t.root.$http(e).then((function(e){n.loading=!1;var t=e.repository.issues,l=t.nodes,a=t.pageInfo,s=t.totalCount;a.hasNextPage||(n.none=!0),n.cursor='"'.concat(a.endCursor,'"'),n.list=n.list.concat(l),n.totalCount=s}))},a=function(){t.root.$loading.show("努力为您查询");var e='query {\n        repository(owner: "qingshanyuluo", name: "qingshanyuluo.github.io") {\n          labels(first: 100) {\n            nodes {\n              name\n            }\n          }\n        }\n      }';t.root.$http(e).then((function(e){n.loading=!1,n.labels=e.repository.labels.nodes,n.labels.length&&(n.label=n.labels[0].name,l())}))},o=function(){n.cursor=null,n.loading=!1,n.none=!1,n.list=[],n.totalCount=0},r=function(e){e.name!==n.label&&(n.label=e.name,o(),l())};return a(),{formatTime:i["b"],getData:l,changeLabel:r,archives:n}}},r=o,c=(n("6153"),n("2877")),f=Object(c["a"])(r,l,a,!1,null,"5f27f560",null);t["default"]=f.exports},6153:function(e,t,n){"use strict";var l=n("1ead"),a=n.n(l);a.a}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["labels"], {
+        "1ead": function (e, t, n) { },
+        "32dc": function (e, t, n) {
+            "use strict";
+            n.r(t);
+            var l = function () {
+                var e = this,
+                    t = e.$createElement,
+                    n = e._self._c || t;
+                return n("div", {
+                    staticClass: "page-labels"
+                }, [e.archives.labels.length ? n("div", {
+                    staticClass: "nav flex flex-middle"
+                }, [n("div", {
+                    staticClass: "name flex flex-center flex-middle"
+                }, [e._v("标签")]), n("div", {
+                    staticClass: "labels flex-item flex"
+                }, e._l(e.archives.labels, (function (t) {
+                    return n("a", {
+                        key: t.name,
+                        staticClass: "label flex flex-middle flex-center",
+                        class: [t.name === e.archives.label && "active"],
+                        attrs: {
+                            href: "javascript:;"
+                        },
+                        domProps: {
+                            textContent: e._s(t.name)
+                        },
+                        on: {
+                            click: function (n) {
+                                return e.changeLabel(t)
+                            }
+                        }
+                    })
+                })), 0)]) : e._e(), n("div", {
+                    staticClass: "list"
+                }, [n("div", {
+                    staticClass: "item"
+                }, [e.archives.label ? n("div", {
+                    staticClass: "item-name flex flex-middle"
+                }, [n("p", {
+                    domProps: {
+                        textContent: e._s(e.archives.label)
+                    }
+                }), n("strong", {
+                    staticClass: "font-clg",
+                    domProps: {
+                        textContent: e._s("( " + e.archives.totalCount + " )")
+                    }
+                })]) : e._e(), n("ul", {
+                    staticClass: "archives"
+                }, e._l(e.archives.list, (function (t) {
+                    return n("li", {
+                        key: t.number,
+                        staticClass: "archive flex flex-middle"
+                    }, [n("span", {
+                        domProps: {
+                            textContent: e._s(e.formatTime(t.createdAt, "MM-dd"))
+                        }
+                    }), n("router-link", {
+                        attrs: {
+                            to: "/archives/" + t.number,
+                            title: t.title
+                        },
+                        domProps: {
+                            textContent: e._s(t.title)
+                        }
+                    }), n("div", {
+                        staticClass: "others flex-item flex-end flex flex-middle"
+                    }, [n("i", {
+                        staticClass: "iconfont icon-comment"
+                    }), n("span", {
+                        domProps: {
+                            textContent: e._s(t.comments.totalCount)
+                        }
+                    })])], 1)
+                })), 0)])]), e.archives.label ? [e.archives.none ? n("div", {
+                    staticClass: "auxi flex flex-middle flex-center"
+                }, [n("i", {
+                    staticClass: "iconfont icon-none"
+                }), n("span", [e._v("目前就这么多啦~")])]) : [e.archives.loading ? [e._m(0)] : [n("div", {
+                    staticClass: "flex flex-middle flex-center"
+                }, [n("a", {
+                    staticClass: "btn-next flex flex-middle flex-center",
+                    attrs: {
+                        href: "javascript:;"
+                    },
+                    on: {
+                        click: e.getData
+                    }
+                }, [e._v("加载更多")])])]]] : e._e()], 2)
+            },
+                a = [function () {
+                    var e = this,
+                        t = e.$createElement,
+                        n = e._self._c || t;
+                    return n("div", {
+                        staticClass: "auxi flex flex-middle flex-center"
+                    }, [n("i", {
+                        staticClass: "iconfont icon-loading"
+                    }), n("span", [e._v("正在加载中")])])
+                }],
+                s = (n("7f7f"), n("750b")),
+                i = n("fa7d"),
+                o = {
+                    setup: function (e, t) {
+                        var n = Object(s["e"])({
+                            list: [],
+                            labels: [],
+                            label: null,
+                            totalCount: 0,
+                            cursor: null,
+                            loading: !1,
+                            none: !1
+                        }),
+                            l = function () {
+                                n.loading = !0;
+                                var e = 'query {\n          repository(owner: "qingshanyuluo", name: "qingshanyuluo.github.io") {\n            issues(filterBy: {labels: "'.concat(n.label, '"}, orderBy: {field: CREATED_AT, direction: DESC},states: OPEN, labels: null, first: 10, after: ').concat(n.cursor, ") {\n              nodes {\n                title\n                createdAt\n                number\n                comments(first: null) {\n                  totalCount\n                }\n              }\n              pageInfo {\n                endCursor\n                hasNextPage\n              }\n              totalCount\n            }\n          }\n        }");
+                                t.root.$http(e).then((function (e) {
+                                    n.loading = !1;
+                                    var t = e.repository.issues,
+                                        l = t.nodes,
+                                        a = t.pageInfo,
+                                        s = t.totalCount;
+                                    a.hasNextPage || (n.none = !0), n.cursor = '"'.concat(a.endCursor, '"'), n.list = n.list.concat(l), n.totalCount = s
+                                }))
+                            },
+                            a = function () {
+                                t.root.$loading.show("努力为您查询");
+                                var e = 'query {\n        repository(owner: "qingshanyuluo", name: "qingshanyuluo.github.io") {\n          labels(first: 100) {\n            nodes {\n              name\n            }\n          }\n        }\n      }';
+                                t.root.$http(e).then((function (e) {
+                                    n.loading = !1, n.labels = e.repository.labels.nodes, n.labels.length && (n.label = n.labels[0].name, l())
+                                }))
+                            },
+                            o = function () {
+                                n.cursor = null, n.loading = !1, n.none = !1, n.list = [], n.totalCount = 0
+                            },
+                            r = function (e) {
+                                e.name !== n.label && (n.label = e.name, o(), l())
+                            };
+                        return a(), {
+                            formatTime: i["b"],
+                            getData: l,
+                            changeLabel: r,
+                            archives: n
+                        }
+                    }
+                },
+                r = o,
+                c = (n("6153"), n("2877")),
+                f = Object(c["a"])(r, l, a, !1, null, "5f27f560", null);
+            t["default"] = f.exports
+        },
+        6153: function (e, t, n) {
+            "use strict";
+            var l = n("1ead"),
+                a = n.n(l);
+            a.a
+        }
+    }
+]);
